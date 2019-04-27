b487ce1793778c07b3a47c72f35338d577e5f0be
daniel mandarano <danielmandarano@yahoo.com.ar>
2019-04-26 21:13:38 -0300

    API

---
+++ Sales.Common/Models/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Sales.Common.Models
+{
+   public class Product
+    {
+        [Key]
+        public int ProductID { get; set; }
+        [Required]
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public bool IsAvailable { get; set; }
+        public DateTime LastBuy { get; set; }
+        public float Stock { get; set; }
+
+    }
+}
